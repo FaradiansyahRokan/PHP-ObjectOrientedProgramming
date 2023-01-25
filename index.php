0a71adb318963328c47cd6a87113cbfdeaa621a0
FaradiansyahRokan <rokansubhi.f@gmail.com>
2023-01-25 09:15:29 +0700

    OOP

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-require_once "Produk/Cek.php";
-require_once "Produk/Game.php";
-require_once "Produk/komik.php";
-require_once "Produk/produk.php";
 
 $game = new Game("Valorant", "orang", "Riot Games", "free", "20");
 $komik = new komik("Naruto", "Masashi Kishimoto", "gtw", 20000, "100");
---
+++ init.php
@@ -0,0 +1,5 @@
+<?php
+require_once "Produk/Cek.php";
+require_once "Produk/Game.php";
+require_once "Produk/komik.php";
+require_once "Produk/produk.php";
