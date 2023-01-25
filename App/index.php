751aea2308f6e1c8b03fea39f3f81cacf973f808
FaradiansyahRokan <rokansubhi.f@gmail.com>
2023-01-25 10:39:39 +0700

    OOP

--- App/index.php
+++ App/index.php
@@ -4,4 +4,6 @@ require "init.php";
 $game = new Game("Valorant", "orang", "Riot Games", "free", "20");
 $komik = new komik("Naruto", "Masashi Kishimoto", "gtw", 20000, "100");
 echo $game->getInfo() . PHP_EOL; 
-echo $komik->getInfo();
+echo $komik->getInfo() . PHP_EOL;
+
+new \App\Produk\User();
--- App/init.php
+++ App/init.php
@@ -3,7 +3,11 @@ require_once "Produk/Cek.php";
 require_once "Produk/Game.php";
 require_once "Produk/komik.php";
 require_once "Produk/produk.php";
+require_once "Produk/User.php";
+require "../Service/User.php";
 
 spl_autoload_register(function($class){
     require_once __DIR__ . '/' . '/Produk/' . $class . '/php';
 });
+
+new User;
