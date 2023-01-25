724d291ac0ed1cfbb842fb6019a132092ff7855b
FaradiansyahRokan <rokansubhi.f@gmail.com>
2023-01-25 09:25:40 +0700

    OOP

--- init.php
+++ init.php
@@ -3,3 +3,7 @@ require_once "Produk/Cek.php";
 require_once "Produk/Game.php";
 require_once "Produk/komik.php";
 require_once "Produk/produk.php";
+
+spl_autoload_register(function($class){
+    require_once __DIR__ . '/' . '/Produk/' . $class . '/php';
+});
