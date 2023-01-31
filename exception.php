d4d602a5c7a32015a11633527e1788cf5835261d
FaradiansyahRokan <rokansubhi.f@gmail.com>
2023-01-31 13:51:26 +0700

    OOP

---
+++ exception.php
@@ -0,0 +1,24 @@
+<?php
+class Connection
+{
+public function connect()
+{
+throw new RuntimeException('Anda harus mengimplement
+asikan method connect() sesuai dengan database driver yang A
+nda gunakan.');
+}
+}
+$connect = new Connection;
+echo $connect->connect();
+
+class Errorable
+{
+public static function calculate(int $number)
+{
+return ($number % 0); //Bila menggunakan $number / 0
+maka hanya akan memunculkan _warning_ bukan _error_
+}
+}
+Errorable::calculate(7);
+echo 'Ini tidak dieksekusi';
+echo PHP_EOL;
