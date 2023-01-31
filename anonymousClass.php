adfc94a83933997f827f1d2b0b909ce48bb660f8
FaradiansyahRokan <rokansubhi.f@gmail.com>
2023-01-31 14:26:04 +0700

    OOP

--- anonymousClass.php
+++ anonymousClass.php
@@ -2,3 +2,9 @@
 interface helloWorld{
     function sayHello(): void;
 }
+$helloWorld = new class implements helloWorld{
+    function sayHello(): void
+    {
+        echo "ini Adalah anonymous Class" . PHP_EOL;
+    }
+};
