0b9b84d3f1f5f780e4d4728d984b5696e9578ddc
FaradiansyahRokan <rokansubhi.f@gmail.com>
2023-02-01 08:19:40 +0700

    OOP

--- variadicFunction.php
+++ variadicFunction.php
@@ -8,6 +8,12 @@ return array_sum($number) . PHP_EOL;
 public static function fuu(int $num){
 var_dump(func_get_args());
 }
+public static function add(int...$numbers){
+    return array_sum($numbers);
+}
 }
 echo Variadic::foo(1, 2, 3, 10);
 Variadic::fuu(10);
+
+$number = [1,2,3];
+echo Variadic::add(...$number);
