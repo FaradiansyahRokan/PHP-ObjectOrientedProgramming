f6a2ea32ce66d17d8f3ae70c55e28ec2948fcefe
FaradiansyahRokan <rokansubhi.f@gmail.com>
2023-02-01 08:06:24 +0700

    OOP

--- variadicFunction.php
+++ variadicFunction.php
@@ -5,5 +5,9 @@ public static function foo(int $number)
 {
 var_dump($number);
 }
+public static function fuu(int $num){
+var_dump(func_get_args());
+}
 }
 Variadic::foo(7, 'ini', 'tetap', 'masuk');
+Variadic::fuu(10);
