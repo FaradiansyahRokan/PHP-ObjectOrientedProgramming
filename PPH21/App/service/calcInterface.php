950195b58c1d7f97a53fd173baa476ee87f2e7b5
FaradiansyahRokan <rokansubhi.f@gmail.com>
2023-02-03 11:29:53 +0700

    OOP

--- PPH21/App/service/calcInterface.php
+++ PPH21/App/service/calcInterface.php
@@ -1,7 +1,8 @@
 <?php
-interface calcInterface{
-    public function calculate($float, $pkp):float;
-    public function maxPkp():float;
-    public function minPkp():float;
-    public function taxPercentage():float;
+interface CalculatorInterface
+{
+public function calculate(float $pkp): float;
+public function maxPkp(): float;
+public function minPkp(): float;
+public function taxPercentage(): float;
 }
