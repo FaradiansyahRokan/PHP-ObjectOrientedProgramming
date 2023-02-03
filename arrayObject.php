daedb1ed704641fb553ed137e5402cfe38170081
FaradiansyahRokan <rokansubhi.f@gmail.com>
2023-02-03 10:45:18 +0700

    OOP

--- arrayObject.php
+++ arrayObject.php
@@ -1,30 +1,30 @@
 <?php
-class ArrayAccessClass implements ArrayAccess
-{
-private $container;
-public function offsetSet($offset, $value)
-{
-if (is_null($offset)) {
-$this->container[] = $value;
-} else {
-$this->container[$offset] = $value;
-}
-}
-public function offsetExists($offset)
-{
-return isset($this->container[$offset]);
-}
-public function offsetUnset($offset)
-{
-unset($this->container[$offset]);
-}
-public function offsetGet($offset)
-{
-return isset($this->container[$offset]) ? $this->container[$offset] : null;
-}
-}
-$object = new ArrayAccessClass();
-$object['name'] = 'Rokan Subhi Faradiansyah';
-$object['address'] = 'In your memory';
-$object['kelas'] = 'X RPL';
-var_dump($object);
+// class ArrayAccessClass implements ArrayAccess
+// {
+// private $container;
+// public function offsetSet($offset, $value)
+// {
+// if (is_null($offset)) {
+// $this->container[] = $value;
+// } else {
+// $this->container[$offset] = $value;
+// }
+// }
+// public function offsetExists($offset)
+// {
+// return isset($this->container[$offset]);
+// }
+// public function offsetUnset($offset)
+// {
+// unset($this->container[$offset]);
+// }
+// public function offsetGet($offset)
+// {
+// return isset($this->container[$offset]) ? $this->container[$offset] : null;
+// }
+// }
+// $object = new ArrayAccessClass();
+// $object['name'] = 'Rokan Subhi Faradiansyah';
+// $object['address'] = 'In your memory';
+// $object['kelas'] = 'X RPL';
+// var_dump($object);
