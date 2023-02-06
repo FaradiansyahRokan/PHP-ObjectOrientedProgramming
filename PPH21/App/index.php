6841efb90ff8b417afffcc17f4f34eb73522f5d8
FaradiansyahRokan <rokansubhi.f@gmail.com>
2023-02-06 08:41:55 +0700

    OOP

--- PPH21/App/index.php
+++ PPH21/App/index.php
@@ -1,1 +1,8 @@
 <?php
+require_once 'App/init.php';
+
+$first = new FirstRuleCalculator();
+$second = new SecondRuleCalculator($first);
+$third = new ThirdRuleCalculator($second);
+$fourth = new FourthRuleCalculator($third);
+
