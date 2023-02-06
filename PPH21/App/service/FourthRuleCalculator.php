1bb1d090dd9827ce7c1c1ff7f71c2979e44a7111
FaradiansyahRokan <rokansubhi.f@gmail.com>
2023-02-06 08:38:09 +0700

    OOP

---
+++ PPH21/App/service/FourthRuleCalculator.php
@@ -0,0 +1,16 @@
+<?php
+class FourthRuleCalculator extends AbstractCalculator
+{
+public function maxPkp(): float
+{
+return 10000000000000000;
+}
+public function minPkp(): float
+{
+return 500000000;
+}
+public function taxPercentage(): float
+{
+return 0.3;
+}
+}
