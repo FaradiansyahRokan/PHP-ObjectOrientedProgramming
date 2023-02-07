30bf53f4842474e85cab2d726357cdf9a8f9a3b1
rokan <rokansubhi.f@gmail.com>
2023-02-07 14:38:50 +0700

    OOP

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pph21/calculator',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5df39a1aa6c0da00e16b9dca78e7fcc7548cd59c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'pph21/calculator' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5df39a1aa6c0da00e16b9dca78e7fcc7548cd59c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
